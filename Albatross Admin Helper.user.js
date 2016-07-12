4bf1d3addc824b3fa665cee9f429d2b4634f4984
isaacpessoa <pessoaisaac@gmail.com>
2016-07-12 16:42:34 -0300

    v 3.0 incompatível com a 2

---
+++ Albatross Admin Helper.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Albatross Admin Helper
+// @namespace    http://<albatross address>/albatross-admin
+// @version      2.0
+// @description  Enhanced occurrence support
+// @author       dilas
+// @match        http://<albatross address>/albatross-admin/occurrences/new*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var metadata = [];
+	var required = null;
+    var descriptionField = $('trix-editor[input="occurrence_description_trix_input_occurrence"]')[0];
+    var actionField = $('trix-editor[input="occurrence_actions_attributes_0_action_trix_input_action"]')[0];
+    var evidenceField = $('trix-editor[input="occurrence_actions_attributes_0_evidence_trix_input_action"]')[0];
+    var proposalField = $('trix-editor[input="occurrence_actions_attributes_0_proposal_trix_input_action"]')[0];
+
+    $('div.row:eq( 2 )').after('<div class="row"><div class="control-group col-xs-12 col-sm-6 col-md-8 col-lg-4"><label class="control-label" for="occurrence_type">Tipo de Ocorrência:</label><div class="controls"><select class="form-control" name="occurrence_type" id="occurrence_type"><option></option></select></div></div></div><br>');
+	
+	 $.getJSON("https://rawgit.com/isaacpessoa/albatross-admin-helper/master/required.json", function(data) {
+        required = data;
+    });
+	
+    $.getJSON("https://rawgit.com/isaacpessoa/albatross-admin-helper/master/meta.json", function(data) {
+        metadata = data;
+
+        $.each(data, function(index, obj) {
+            $('#occurrence_type').append(new Option(obj.title, index));
+        });
+    });
+
+    $('#occurrence_type').on('change', function (evt) {
+        var optionSelected = $('option:selected', this);
+        var valueSelected = this.value;
+
+        descriptionField.editor.insertHTML(metadata[valueSelected].description);
+		descriptionField.editor.insertHTML(required.description);
+        actionField.editor.insertHTML(required.action);
+		evidenceField.editor.insertHTML(metadata[valueSelected].tEvidence);
+		evidenceField.editor.insertHTML(required.evidence);
+        evidenceField.editor.insertHTML(metadata[valueSelected].evidence);
+        proposalField.editor.insertHTML(metadata[valueSelected].proposal);
+    });
+
+})();
